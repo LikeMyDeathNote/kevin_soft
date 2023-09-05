beb4f761e5bb54999dd6c55dac603fe64ff93db7
LikeMyDeathNote <LikeMyDeathNote@users.noreply.github.com>
2023-09-05 18:14:47 +0200

    test

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hey Kevin.. slecht nieuws..");
+    println!("Jacco is gay en pakt ons 1 voor 1 van achteren zonder warning..");
+    println!("Ren nu dat je nog kan!!");
+    loop{}
+    
+}
