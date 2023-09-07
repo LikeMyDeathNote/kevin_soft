a83c79307fc1c0f0d26d714a48e7b59ebc09b1cf
LikeMyDeathNote <LikeMyDeathNote@users.noreply.github.com>
2023-09-08 01:48:42 +0200

    Jacco Was Here

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,38 @@ use bevy::core_pipeline::clear_color::ClearColorConfig;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Jacco Liep Het Raam Open".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: true,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle {
         camera_2d: Camera2d {
             clear_color: ClearColorConfig::Custom(Color::GREEN),
         },
         ..default()
     });
+    let texture = asset_server.load("Jacco.png");
     commands.spawn(SpriteBundle {
         sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0, 100.0)),
+            custom_size: Some(Vec2::new(250.0, 250.0)),
             ..default()
         },
+        texture,
         ..default()
     });
 }
