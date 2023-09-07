4f73330242d15b329abd9bc3ac9ba3fb7c1e6811
LikeMyDeathNote <LikeMyDeathNote@users.noreply.github.com>
2023-09-07 21:56:36 +0200

    The First Sprite Has Been Rendered!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use bevy::prelude::*;
+use bevy::core_pipeline::clear_color::ClearColorConfig;
 
 fn main() {
-   App::new()
-       .add_systems(Update, hello_world_system)
-       .run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
 
-fn hello_world_system() {
-   println!("hello world");
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        camera_2d: Camera2d {
+            clear_color: ClearColorConfig::Custom(Color::GREEN),
+        },
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(100.0, 100.0)),
+            ..default()
+        },
+        ..default()
+    });
 }
